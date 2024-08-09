7282f720de9bcad3b236a7e3d95ba1687d8d9b28
Sagar Pingale <sagarpingale0110@gmail.com>
2024-08-09 11:07:45 +0000

    added structure folder

--- src/include/structure/database_manager.hpp
+++ src/include/structure/database_manager.hpp
@@ -5,11 +5,26 @@
 #include "../output/output.hpp"
 
 class DatabaseManager {
-public:
+private:
     std::vector<Database> database_array;
     Database* current_database;
 
+public:
     DatabaseManager() : database_array(), current_database(nullptr) {}
+
+    bool set_current(std::string name);
+
+    std::string get_current();
+
+    std::vector<std::string> get_databases();
+
+    bool remove(std::string name);
+
+    bool add(std::string name);
+
+    bool isset();
+
+    bool exists(std::string name);
 };
 
 extern DatabaseManager dbm;
--- src/query/database.cpp
+++ src/query/database.cpp
@@ -1,73 +1,58 @@
 #include "../include/query/database.hpp"
 
 Response createDatabase(node* root){
-
     // use database statement -> identifier -> name
     std::string name = root->children[2]->children[0]->token;
 
     // check if already exists
-    for (const auto& db : dbm.database_array) {
-        if (db.name == name) {
-            return databaseErrorMessage("duplicate");
-        }
+    if(dbm.exists(name)){
+        return databaseErrorMessage("duplicate");
     }
 
-    Database new_database(name);
-    dbm.database_array.push_back(new_database);
-
-        return successMessage("OK");
+    dbm.add(name);
+    return successMessage("OK");
 };
 
 Response dropDatabase(node* root){
-
     // use database statement -> identifier -> name
     std::string name = root->children[2]->children[0]->token;
 
-    for (auto it = dbm.database_array.begin(); it != dbm.database_array.end(); it++) {
-
-        if (it->name == name) {
-            if (dbm.current_database && dbm.current_database->name == name) {
-                dbm.current_database = nullptr;
-            }
-
-            dbm.database_array.erase(it);
-
-            return successMessage("OK");
-        }
+    // check if exists
+    if(dbm.exists(name)){
+        dbm.remove(name);
+        return successMessage("OK");
     }
 
     return databaseErrorMessage("notfound");
 };
 
 Response showDatabase() {
-
     std::vector<std::vector<std::string>> simple_table;
 
-    for (const auto& db : dbm.database_array) {
-        simple_table.push_back({db.name});
+    std::vector<std::string> databases = dbm.get_databases();
+
+    for (const auto& db : databases) {
+        simple_table.push_back({db});
     }
 
-    simpleTablePrint({"show databases;"},simple_table);
+    simpleTablePrint({"show databases"},simple_table);
 
     return successMessage("OK");
 }
 
 Response useDatabase(node* root) {
-
     // use database statement -> identifier -> name
     std::string name = root->children[2]->children[0]->token;
 
-    if (dbm.current_database && dbm.current_database->name == name) {
+    if (dbm.get_current() == name) {
         return databaseErrorMessage("alreadyusing");
     }
 
-    for (auto& db : dbm.database_array) {
-        if (db.name == name) {
-            dbm.current_database = &db;
-            return successMessage("OK");
-        }
+    if(!dbm.exists(name)){
+        return databaseErrorMessage("notfound");
     }
 
-    return databaseErrorMessage("notfound");
+    dbm.set_current(name);
+    return successMessage("OK");
 }
 
